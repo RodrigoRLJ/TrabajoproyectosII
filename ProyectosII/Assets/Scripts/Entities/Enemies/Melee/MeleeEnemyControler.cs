1d6e744c554b498921fbd8834019f3c3f75aab1d
RodrigoRLJ <rlopezjimenez780@gmail.com>
2023-11-05 18:05:30 +0100

    enemigo patrulla

---
+++ ProyectosII/Assets/Scripts/Entities/Enemies/Melee/MeleeEnemyControler.cs
@@ -0,0 +1,86 @@
+using Entities.EnemyMelee;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeleeEnemyControler : MonoBehaviour
+{
+    [SerializeField] public EnemyMeleeStats stats;
+    public List<Transform> coordenada;
+    public Transform Player;
+    private Rigidbody2D RigidBody2D;
+
+    int siguienteCoordenada = 0;
+    int cambioCoordenada = 1;
+    int distancia = 8;    
+    int velocidadpatrullando = 1;
+    int velocidadAtacando = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RigidBody2D = GetComponent<Rigidbody2D>();
+
+    }
+    //void Init()
+    //{
+    //GameObject waypoints = new GameObject("waypoints");
+
+    //GameObject p1 = new GameObject("coordenada1");
+    //p1.transform.SetParent(waypoints.transform);
+    //GameObject p2 = new GameObject("coordenada2");
+    //p2.transform.SetParent(waypoints.transform);
+    //coordenada = new List<Transform>();
+    //coordenada.Add(p1.transform);
+    //coordenada.Add(p2.transform);
+    //}
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player);
+        if ( Vector3.Distance(transform.position, Player.position) >= distancia)
+        {
+            RigidBody2D.velocity = new Vector2(velocidadAtacando, RigidBody2D.velocity.y);
+
+            if (Vector3.Distance(transform.position, Player.position) == 0)
+            {
+                //atacar
+            }
+
+        }
+        else
+        {
+            patrulla();
+            
+
+        }
+    }
+
+    void patrulla()
+    {
+        Transform objetivo = coordenada[siguienteCoordenada];
+        if(objetivo.transform.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, objetivo.position, velocidadpatrullando * Time.deltaTime);
+        if(Vector3.Distance(transform.position,objetivo.position) <1f)
+        {
+            if (siguienteCoordenada == 1)
+            {
+                cambioCoordenada = -1;
+            }
+            if (siguienteCoordenada == 0)
+            {
+                cambioCoordenada = 1;
+            }
+            siguienteCoordenada += cambioCoordenada;
+        }
+    }
+}
---
+++ ProyectosII/Assets/Scripts/Entities/Enemies/Melee/Scriptable Objects/EnemyMeleeStats.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Entities.EnemyMelee
+{
+    [CreateAssetMenu(fileName = "EnemyMeleeStats",
+        menuName = "Scriptable Objects/Enemy Stats/Enemy Melee Stats")]
+    public class EnemyMeleeStats : ScriptableObject
+    {
+        //Distancia a la que atacará al jugador
+        [SerializeField] public float distanceToPlayer;
+
+        //velocidad que tendra al patrullar
+        [SerializeField] public float patrolSpeed;
+
+        //velocidad que tendra al atacar al jugador
+        [SerializeField] public int attackSpeed;
+
+        //vida del monstruo
+        [SerializeField] public float health;
+
+        
+    }
+}
