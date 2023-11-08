46cdf05e0d09fc7cf4dced64a5f83d24bd87a3c1
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2023-11-08 10:50:34 +0100

    Patches
    Pequeños ajustes de los niveles y la UI

--- ProyectosII/Assets/Scripts/UserInterface/SceneUIController.cs
+++ ProyectosII/Assets/Scripts/UserInterface/SceneUIController.cs
@@ -2,9 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelSelectorController : MonoBehaviour
-
-
+public class SceneUIController : MonoBehaviour
 {
     public void ReturnMainMenu()
     {
@@ -29,5 +27,20 @@ public class LevelSelectorController : MonoBehaviour
     public void GoToLevel4()
     {
         GameManager.ChangeScene("Level4");
+    }
+
+    public void GoToLevelSelector()
+    {
+        GameManager.ChangeScene("LevelSelector");
+    }
+
+    public void GoToEnciclopedia()
+    {
+        GameManager.ChangeScene("Enciclopedia");
+    }
+
+    public void GoToCredits()
+    {
+        GameManager.ChangeScene("Credits");
     }
 }
