d87505bc3c360aeced4c03f0a205fc43ac5a3117
Guillecasaus <guille.casaus@gmail.com>
2023-11-10 19:07:47 +0100

    Script non fixes

--- ProyectosII/Assets/Scripts/GameObjects/damagePlayer.cs
+++ ProyectosII/Assets/Scripts/GameObjects/damagePlayer.cs
@@ -6,9 +6,10 @@ public class damagePlayer : MonoBehaviour
 {
     private void OnTriggerStay2D(Collider2D other) {
         if (other.CompareTag("Player")) {
-            
+//            if(other.GetComponent<PlayerController>().PlayerChangeHealth(-10);
+
             //other.PlayerChangeHealth(-10);
-            ((PlayerController)other.transform).PlayerChangeHealth(-10);
+  //          ((PlayerController)other.transform).PlayerChangeHealth(-10);
         }
     }
 }
