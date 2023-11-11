84a0093ee07a3135311e206fe0374b334f79da3f
Guillecasaus <guille.casaus@gmail.com>
2023-11-11 16:03:43 +0100

    Cambio en la vida del persona por GameObjects

--- ProyectosII/Assets/Scripts/GameObjects/colisionManzana.cs
+++ ProyectosII/Assets/Scripts/GameObjects/colisionManzana.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Entities.Player;
 
 public class colisionManzana : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.collider.CompareTag("Player")){
-            Debug.Log("Colisión");
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerController>().PlayerChangeHealth(+10);
         }
     }
 }
--- ProyectosII/Assets/Scripts/GameObjects/damagePlayer.cs
+++ ProyectosII/Assets/Scripts/GameObjects/damagePlayer.cs
@@ -5,11 +5,9 @@ using Entities.Player;
 public class damagePlayer : MonoBehaviour
 {
     private void OnTriggerStay2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
-//            if(other.GetComponent<PlayerController>().PlayerChangeHealth(-10);
+         if (other.CompareTag("Player")) {
+             other.GetComponent<PlayerController>().PlayerChangeHealth(-10);
+         }
+     }
 
-            //other.PlayerChangeHealth(-10);
-  //          ((PlayerController)other.transform).PlayerChangeHealth(-10);
-        }
-    }
 }
