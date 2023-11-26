29f79c2ae76232a5e7dc439fbebd48eeb538db37
Guillecasaus <guille.casaus@gmail.com>
2023-11-26 18:19:44 +0100

    Fondo

--- ProyectosII/Assets/Scripts/Entities/Camera/FondoMovimiento.cs
+++ ProyectosII/Assets/Scripts/Entities/Camera/FondoMovimiento.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class FondoMovimiento : MonoBehaviour{
    
-    [SerializeField] private Vector2 velocidadMovimiento;
+    [SerializeField] private GameObject camara;
 
     private Vector2 offset;
     private Material material;
     private Rigidbody2D jugador;
 
-    private void Awake(){
+  /*  private void Awake(){
         material = GetComponent<SpriteRenderer>().material;
         jugador = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-    }
+    }*/
     
-    private void Update() { 
-    
-         offset = (jugador.velocity.x * 0.1f) * velocidadMovimiento * Time.deltaTime;
-         material.mainTextureOffset += offset;
+    private void Update() {
+
+        if (camara != null) { 
+            this.transform.position = camara.transform.position;
+        }
+
+       //  offset = (jugador.velocity.x * 0.1f) * velocidadMovimiento * Time.deltaTime;
+      //   material.mainTextureOffset += offset;
 
     }
 }
