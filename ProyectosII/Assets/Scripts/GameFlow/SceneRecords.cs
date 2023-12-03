41a2207f7dbdac91be89eb112c0c733cd8eaa1b5
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2023-12-03 18:49:56 +0100

    Small patches

--- ProyectosII/Assets/Scripts/GameFlow/SceneRecords.cs
+++ ProyectosII/Assets/Scripts/GameFlow/SceneRecords.cs
@@ -11,5 +11,5 @@ public class SceneRecords
     public const string LEVEL_1 = "Level1";
     public const string LEVEL_2 = "Level2";
     public const string LEVEL_3 = "Level3";
-    public const string LEVEL_4 = "PruebaGuillermo";
+    public const string LEVEL_4 = "Level4";
 }
